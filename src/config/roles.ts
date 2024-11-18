237fccac8ec35572a5c7d5c695800fefa792bc89
k-leb <ecommercepearl05@gmail.com>
2024-11-18 18:49:09 +0100

    feat: worked on admin login

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -2,13 +2,13 @@ const allRoles = {
   user: ['getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
     'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
     'manageOrder', 'getOrders', 'manageSellers', 'verify-and-create-order',
-    'become-seller','follow','unfollow','followers','userProducts'
+    'become-seller','follow','unfollow','followers','userProducts','adminlogin'
   ],
-  admin: ['manageSellers'],
+  admin: ['manageSellers','adminlogin'],
   seller: ['getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
     'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
     'manageOrder', 'getOrders', 'manageSellers', 'verify-and-create-order',
-    'become-seller','follow','unfollow','followers','userProducts'
+    'become-seller','follow','unfollow','followers','userProducts','adminlogin'
   ]
 };
 
