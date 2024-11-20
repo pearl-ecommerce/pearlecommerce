36c17e62710aa3d2bd10c54cf1711b0b86e5f4da
k-leb <ecommercepearl05@gmail.com>
2024-11-20 07:49:23 +0100

    feat: add role

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -2,13 +2,17 @@ const allRoles = {
   user: ['getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
     'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
     'manageOrder', 'getOrders', 'manageSellers', 'verify-and-create-order',
-    'become-seller','follow','unfollow','followers','userProducts','adminlogin'
+    'become-seller', 'follow', 'unfollow', 'followers', 'userProducts', 'adminlogin',
+    'manageCart','getCarts','removecart',
   ],
-  admin: ['manageSellers','adminlogin','adminuser','profile','logout','manageUsers','getUsers','activate','deactivate'],
+  admin: ['manageSellers', 'adminlogin', 'adminuser', 'profile', 'logout', 'manageUsers', 'getUsers', 'activate', 'deactivate',
+    'manageCart','getCarts','removecart', 
+  ],
   seller: ['getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
     'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
     'manageOrder', 'getOrders', 'manageSellers', 'verify-and-create-order',
-    'become-seller','follow','unfollow','followers','userProducts','adminlogin'
+    'become-seller', 'follow', 'unfollow', 'followers', 'userProducts', 'adminlogin',
+    'manageCart','getCarts','removecart',
   ]
 };
 
