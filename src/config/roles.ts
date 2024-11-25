cbdfd334aa1e135c907897c45ebe1e13345c30d4
k-leb <ecommercepearl05@gmail.com>
2024-11-25 16:30:52 +0100

    feat: worked on discount

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -8,7 +8,14 @@ const allRoles = {
     'manageDiscount','getCarts'
   ],
   admin: ['manageSellers', 'adminlogin', 'adminuser', 'profile', 'logout', 'manageUsers', 'getUsers', 'activate', 'deactivate',
-    'manageCart','getCarts','removecart', 'managePricing','getPricing','manageDiscount','getCarts',"userProducts",'manageProducts'
+    'manageCart','getCarts','removecart', 'managePricing','getPricing','manageDiscount','getCarts',"userProducts",'manageProducts',
+    'getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
+    'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
+    'manageOrder', 'getOrders', 'manageSellers', 'verify-and-create-order',
+    'become-seller', 'follow', 'unfollow', 'followers', 'userProducts', 'adminlogin',
+    'manageCart', 'getCarts', 'removecart',
+    'managePricing', 'getPricing',
+    'manageDiscount','getCarts'
   ],
   seller: ['getProducts', 'searchProducts', 'manageProducts', 'getUsers', 'manageSellers', 'getStores', 'manageSellers', 'manageUsers',
     'manageCategories', 'getCategories', 'managePayments', 'getPayments', 'managePayments',
