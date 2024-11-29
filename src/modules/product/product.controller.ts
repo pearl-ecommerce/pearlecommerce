42bbb09ae3498b9a380c28ed69c481f17e17eb8f
k-leb <ecommercepearl05@gmail.com>
2024-11-29 11:09:41 +0100

    feat: worked on discount

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -148,8 +148,8 @@ const userId = req.body;
 export const userProducts = catchAsync(async (req: Request, res: Response) => {
   // Check if `userId` is provided as a string in the request parameters
   // const userId = req.body;
-  const userId = req.query['userId'] as string;
-  if (typeof userId == 'string') {
+  const userId = req.body;
+  if (req.body.userId == 'string') {
     console.log("User ID received:", req.params['userId']);
 
     // Call the service function to fetch products created by the user
