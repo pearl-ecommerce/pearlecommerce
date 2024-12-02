1792889dbb8ed0f5fdc869a8ab531155204ec8e3
k-leb <ecommercepearl05@gmail.com>
2024-12-02 17:43:45 +0100

    feat: worked on cart

--- src/modules/user/user.interfaces.ts
+++ src/modules/user/user.interfaces.ts
@@ -40,8 +40,8 @@ export interface IUser {
   active: boolean;
   followers: mongoose.Types.ObjectId[];
   following: mongoose.Types.ObjectId[];
-  userId: mongoose.Types.ObjectId; 
   imageUrl: string;
+  userId?: string;
 }
 
 export interface IUserDoc extends IUser, Document {
